8ccc036d0a9f4516ea4dd6ecaf64632f73eb2b43
Andy Situ <and.situ@gmail.com>
2020-12-10 12:32:50 -0800

    Try authenticating with filestream

--- src/main/java/com/wms/wms_server/firebase/FirebaseInitialize.java
+++ src/main/java/com/wms/wms_server/firebase/FirebaseInitialize.java
@@ -16,12 +16,17 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.cloud.FirestoreClient;
 
+import java.io.FileInputStream;
+import java.io.InputStream;
+
 @Service
 public class FirebaseInitialize {
     @PostConstruct
     public void initialize() throws IOException {
+        InputStream serviceAccount = new FileInputStream("./wms-server-firestore.json");
         FirebaseOptions options = new FirebaseOptions.Builder()
-            .setCredentials(GoogleCredentials.getApplicationDefault())
+            // .setCredentials(GoogleCredentials.getApplicationDefault())
+            .setCredentials(GoogleCredentials.fromStream(serviceAccount))
             .build();
 
         FirebaseApp.initializeApp(options);
