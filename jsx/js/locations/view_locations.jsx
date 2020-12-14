ae195ec8771dc3a28ddfde05719732f69520f9ae
Andy Situ <and.situ@gmail.com>
2020-12-14 15:37:19 -0800

    change create btn to + (svg)

--- jsx/js/locations/view_locations.jsx
+++ jsx/js/locations/view_locations.jsx
@@ -41,9 +41,11 @@ class LocationTable extends React.Component {
 
   render () {
     return (<div>
-      <button className="btn btn-primary"
-        onClick={this.show_menu}
-      >Create</button>
+      <button className="btn btn-sm btn-primary" onClick={this.show_menu}>
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
+          <path fill-rule="evenodd" d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+        </svg>
+      </button>
 
       <table className="table table-sm">
         <thead>
--- src/main/resources/static/js/locations/view_locations.js
+++ src/main/resources/static/js/locations/view_locations.js
@@ -63,10 +63,12 @@ var LocationTable = function (_React$Component) {
         null,
         React.createElement(
           "button",
-          { className: "btn btn-primary",
-            onClick: this.show_menu
-          },
-          "Create"
+          { className: "btn btn-sm btn-primary", onClick: this.show_menu },
+          React.createElement(
+            "svg",
+            { xmlns: "http://www.w3.org/2000/svg", width: "16", height: "16", fill: "currentColor", "class": "bi bi-plus", viewBox: "0 0 16 16" },
+            React.createElement("path", { "fill-rule": "evenodd", d: "M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" })
+          )
         ),
         React.createElement(
           "table",
