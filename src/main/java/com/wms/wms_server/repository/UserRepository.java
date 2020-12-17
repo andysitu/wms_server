fdbdf68c4d5a1e844e190cbe58713835a5d3de57
Andy Situ <and.situ@gmail.com>
2020-12-16 18:19:34 -0800

    Create UserRepository.java

---
+++ src/main/java/com/wms/wms_server/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.wms.wms_server.repository;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.wms.wms_server.model.user.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String>{    
+}
