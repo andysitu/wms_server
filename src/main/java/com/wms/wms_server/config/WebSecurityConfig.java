30104e75e71b6534c722d879bc63116013b679c9
Andy Situ <and.situ@gmail.com>
2020-12-17 20:15:19 -0800

    Remove unused imports from WebSecurityConfig

--- src/main/java/com/wms/wms_server/config/WebSecurityConfig.java
+++ src/main/java/com/wms/wms_server/config/WebSecurityConfig.java
@@ -1,12 +1,10 @@
 package com.wms.wms_server.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 @Configuration
 @EnableWebSecurity
