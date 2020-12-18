608f6a6032aa6f25ef41013e96a5c1d231a83188
Andy Situ <and.situ@gmail.com>
2020-12-17 18:54:09 -0800

    Update WebSecurityConfig.java

--- src/main/java/com/wms/wms_server/config/WebSecurityConfig.java
+++ src/main/java/com/wms/wms_server/config/WebSecurityConfig.java
@@ -1,35 +1,28 @@
-// package com.wms.wms_server.config;
+package com.wms.wms_server.config;
 
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
-// import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
-// import com.wms.wms_server.config.WmsAuthenticationProvider;
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-
-// @Configuration
-// @EnableWebSecurity
-// public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-//     @Autowired
-//     private WmsAuthenticationProvider wmsAuthenticationProvider;
-
-//     @Override
-//     protected void configure(HttpSecurity http) throws Exception {
-//         http.authorizeRequests()
-//                 // .antMatchers("/view_locations").permitAll()
-//                 .antMatchers("/").authenticated()
-//                 // .anyRequest().authenticated()
-//                 .and()
-//             .formLogin()
-//                 .loginPage("/login")
-//                 .permitAll()
-//                 .and()
-//             .logout()
-//                 .permitAll();
-//         http.authenticationProvider(wmsAuthenticationProvider);
-//     }
-// }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests(a -> a
+            .antMatchers("/", "/error", "/login").permitAll()
+            .anyRequest().authenticated()
+        )
+        .exceptionHandling( e-> {
+            System.out.println(e);
+            e.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED));
+        })
+        .oauth2Login()
+            .loginPage("/login");
+    }
+}
