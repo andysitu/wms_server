3af91dd729a33fd59eb17439043f2726f65c537a
Andy Situ <and.situ@gmail.com>
2020-12-17 16:01:17 -0800

    Get User API (from OAuth)

--- src/main/java/com/wms/wms_server/WmsServerApplication.java
+++ src/main/java/com/wms/wms_server/WmsServerApplication.java
@@ -4,10 +4,23 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.Map;
 
 @SpringBootApplication
 @EnableMongoAuditing
+@RestController
 public class WmsServerApplication {
+	@GetMapping("/user") 
+	public  Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
+		return Collections.singletonMap("name", principal.getAttribute("name"));
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(WmsServerApplication.class, args);
 	}
