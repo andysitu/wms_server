4cec679a45210e6dd17b3c7685c464332f5239f3
Andy Situ <and.situ@gmail.com>
2020-12-17 21:06:59 -0800

    Create UserController & move '/user' api to there

--- src/main/java/com/wms/wms_server/WmsServerApplication.java
+++ src/main/java/com/wms/wms_server/WmsServerApplication.java
@@ -4,14 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Map;
 
 import com.wms.wms_server.services.OAuthUserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,12 +16,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 public class WmsServerApplication {
 	@Autowired
 	OAuthUserService oAuthUserService;
-	
-	@GetMapping("/user")
-	@ResponseBody
-	public  Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
-		return oAuthUserService.getUser(principal);
-	}
 
 	@GetMapping(value={"/error"})
     public String view_error() {
---
+++ src/main/java/com/wms/wms_server/controllers/UserController.java
@@ -0,0 +1,26 @@
+package com.wms.wms_server.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.wms.wms_server.services.OAuthUserService;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Map;
+
+@RestController
+public class UserController {
+    @Autowired
+    OAuthUserService oAuthUserService;
+    
+    @GetMapping("/user")
+	@ResponseBody
+	public  Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
+		return oAuthUserService.getUser(principal);
+	}
+}
