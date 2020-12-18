bd5f5e3c49d4a125403c3b5b267b83db51ee7879
Andy Situ <and.situ@gmail.com>
2020-12-17 18:53:54 -0800

    Comment out login page that redirected to Google login

--- src/main/java/com/wms/wms_server/controllers/OAuth2LoginController.java
+++ src/main/java/com/wms/wms_server/controllers/OAuth2LoginController.java
@@ -10,13 +10,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class OAuth2LoginController {
-    @GetMapping("/")
-	public String index(Model model,
-						@RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient authorizedClient,
-						@AuthenticationPrincipal OAuth2User oauth2User) {
-		model.addAttribute("userName", oauth2User.getName());
-		model.addAttribute("clientName", authorizedClient.getClientRegistration().getClientName());
-		model.addAttribute("userAttributes", oauth2User.getAttributes());
-		return "index";
-	}
+    // @GetMapping("/login")
+	// public String index(Model model,
+	// 					@RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient authorizedClient,
+	// 					@AuthenticationPrincipal OAuth2User oauth2User) {
+	// 	model.addAttribute("userName", oauth2User.getName());
+	// 	model.addAttribute("clientName", authorizedClient.getClientRegistration().getClientName());
+	// 	model.addAttribute("userAttributes", oauth2User.getAttributes());
+	// 	return "index";
+	// }
 }
