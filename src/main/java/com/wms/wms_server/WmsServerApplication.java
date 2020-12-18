7cd0e33253f57893b3deee1ffdb6fc4c263b9cb5
Andy Situ <and.situ@gmail.com>
2020-12-17 18:54:19 -0800

    Add error, index, login pages

--- src/main/java/com/wms/wms_server/WmsServerApplication.java
+++ src/main/java/com/wms/wms_server/WmsServerApplication.java
@@ -6,7 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
@@ -16,15 +18,29 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 @SpringBootApplication
 @EnableMongoAuditing
-@RestController
+@Controller
 public class WmsServerApplication {
 	@Autowired
 	OAuthUserService oAuthUserService;
 	
-	@GetMapping("/user") 
+	@GetMapping("/user")
+	@ResponseBody
 	public  Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
 		return oAuthUserService.getUser(principal);
 	}
+
+	@GetMapping(value={"/error"})
+    public String view_error() {
+        return "error";
+	}
+	@GetMapping(value={"/index"})
+    public String view_index() {
+        return "index";
+	}
+	@GetMapping(value={"/login"})
+    public String view_login() {
+        return "login";
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(WmsServerApplication.class, args);
