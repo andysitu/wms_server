f372c0a84dd50f9f09b15a3da7655690acd7a9bc
Andy Situ <and.situ@gmail.com>
2020-12-18 22:22:21 -0800

    Update UserRepository.java

--- src/main/java/com/wms/wms_server/repository/UserRepository.java
+++ src/main/java/com/wms/wms_server/repository/UserRepository.java
@@ -8,4 +8,5 @@ import com.wms.wms_server.model.user.User;
 @Repository
 public interface UserRepository extends MongoRepository<User, String>{    
     User findByName(String name);
+    User findBySub(String sub);
 }
