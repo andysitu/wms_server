bfbec8aab429df690e46ce9cca222a3f7d251a8b
Andy Situ <and.situ@gmail.com>
2020-12-18 17:33:46 -0800

    Update get_login.js-ajaxSetup uses CSRF token for request header

--- src/main/resources/static/js/layout/get_login.js
+++ src/main/resources/static/js/layout/get_login.js
@@ -1,3 +1,26 @@
+$.ajaxSetup({
+  beforeSend: function beforeSend(xhr, settings) {
+    // Get CSRF token
+    if (settings.type == "POST" || settings.type == "PUT" || settings.type == "DELETE" || settings.type == "PATCH") {
+      if (!(/^http:.*/.test(settings.url) || /^https:.*/.test(settings.url))) {
+        var cookies = document.cookie.split(";"),
+            c,
+            token;
+        for (var i = 0; i < cookies.length; i++) {
+          c = cookies[i].split("=");
+          if (c[0] == "XSRF-TOKEN") {
+            token = c[1];
+            break;
+          }
+        }
+        if (token) {
+          xhr.setRequestHeader("X-XSRF-TOKEN", token);
+        }
+      }
+    }
+  }
+});
+
 var log_helper = {
   logout() {
     $.ajax({
