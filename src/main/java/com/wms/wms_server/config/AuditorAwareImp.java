8f08d10513b8ebef7db82391f8b7eeba0ab3e811
Andy Situ <and.situ@gmail.com>
2020-12-20 13:43:50 -0800

    Update AuditorAwareImp.java

--- src/main/java/com/wms/wms_server/config/AuditorAwareImp.java
+++ src/main/java/com/wms/wms_server/config/AuditorAwareImp.java
@@ -4,12 +4,17 @@ import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
 import java.util.Optional;
 
 public class AuditorAwareImp implements AuditorAware<String> {
+    //  AuditorAware is used for @CreatedBy & @LastModifiedBy
     @Override
     public Optional<String> getCurrentAuditor() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        System.out.println(authentication.getPrincipal());
+
         return Optional.of(authentication.getName());
     }
 }
