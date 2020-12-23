8f9b1888482ad8abe655ed26dddde40a0747e64d
Andy Situ <and.situ@gmail.com>
2020-12-22 19:51:22 -0800

    Update AuditingConfig.java

--- src/main/java/com/wms/wms_server/config/AuditingConfig.java
+++ src/main/java/com/wms/wms_server/config/AuditingConfig.java
@@ -6,7 +6,7 @@ import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
-@EnableJpaAuditing
+@EnableJpaAuditing(auditorAwareRef = "auditorProvider")
 public class AuditingConfig {
     @Bean
     public AuditorAware<String> auditorProvider() {
