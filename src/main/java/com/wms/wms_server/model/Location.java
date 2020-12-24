1042f645ce9d562db98dbf555405e70b8d848877
Andy Situ <and.situ@gmail.com>
2020-12-23 18:05:09 -0800

    Enable auditing for Location

--- src/main/java/com/wms/wms_server/model/Location.java
+++ src/main/java/com/wms/wms_server/model/Location.java
@@ -1,6 +1,7 @@
 package com.wms.wms_server.model;
 
 import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -10,10 +11,12 @@ import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.util.Date;
 
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 public class Location {
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
