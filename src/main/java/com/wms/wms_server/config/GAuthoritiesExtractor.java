9a7bdbfd2c79405bdaa6de57e6d5dcf6551c0f4d
Andy Situ <and.situ@gmail.com>
2020-12-23 20:39:08 -0800

    Disable Google Authority extractor attempt

--- src/main/java/com/wms/wms_server/config/GAuthoritiesExtractor.java
+++ src/main/java/com/wms/wms_server/config/GAuthoritiesExtractor.java
@@ -1,29 +1,29 @@
-package com.wms.wms_server.config;
+// package com.wms.wms_server.config;
 
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.LinkedHashSet;
-import java.util.List;
-import java.util.Map;
+// import java.util.ArrayList;
+// import java.util.LinkedHashMap;
+// import java.util.LinkedHashSet;
+// import java.util.List;
+// import java.util.Map;
 
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.AuthorityUtils;
-import org.springframework.boot.autoconfigure.security.oauth2.resource.AuthoritiesExtractor;
+// import org.springframework.security.core.GrantedAuthority;
+// import org.springframework.security.core.authority.AuthorityUtils;
+// import org.springframework.boot.autoconfigure.security.oauth2.resource.AuthoritiesExtractor;
 
-public class GAuthoritiesExtractor implements AuthoritiesExtractor{
-    @Override
-    public List<GrantedAuthority> extractAuthorities(Map<String, Object> map) {
-        return AuthorityUtils.commaSeparatedStringToAuthorityList(asAuthorities(map));
-    }
+// public class GAuthoritiesExtractor implements AuthoritiesExtractor{
+//     @Override
+//     public List<GrantedAuthority> extractAuthorities(Map<String, Object> map) {
+//         return AuthorityUtils.commaSeparatedStringToAuthorityList(asAuthorities(map));
+//     }
 
-    private String asAuthorities(Map<String, Object> map) {
-        List<String> authorities = new ArrayList<>();
-        authorities.add("ADMIN");
-        List<LinkedHashMap<String, String>> a = 
-            (List<LinkedHashMap<String, String>>) map.get("authorities");
-        for (LinkedHashMap<String, String> entry: a) {
-            authorities.add(entry.get("authority"));
-        }
-        return String.join(",", authorities);
-    }
-}
+//     private String asAuthorities(Map<String, Object> map) {
+//         List<String> authorities = new ArrayList<>();
+//         authorities.add("ADMIN");
+//         List<LinkedHashMap<String, String>> a = 
+//             (List<LinkedHashMap<String, String>>) map.get("authorities");
+//         for (LinkedHashMap<String, String> entry: a) {
+//             authorities.add(entry.get("authority"));
+//         }
+//         return String.join(",", authorities);
+//     }
+// }
--- src/main/java/com/wms/wms_server/config/GPrincipalExtractor.java
+++ src/main/java/com/wms/wms_server/config/GPrincipalExtractor.java
@@ -1,38 +1,38 @@
-package com.wms.wms_server.config;
+// package com.wms.wms_server.config;
 
-import java.util.Map;
+// import java.util.Map;
 
-import org.springframework.boot.autoconfigure.security.oauth2.resource.PrincipalExtractor;
+// import org.springframework.boot.autoconfigure.security.oauth2.resource.PrincipalExtractor;
 
-import com.wms.wms_server.model.user.WMSUser;
-import com.wms.wms_server.services.UserService;
+// import com.wms.wms_server.model.user.WMSUser;
+// import com.wms.wms_server.services.UserService;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.stereotype.Component;
 
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.core.OAuth2AccessToken;
+// import org.springframework.security.core.Authentication;
+// import org.springframework.security.core.context.SecurityContextHolder;
+// import org.springframework.security.oauth2.core.OAuth2AccessToken;
 
-@Component
-public class GPrincipalExtractor implements PrincipalExtractor{
-    @Autowired
-    private UserService userService;
+// @Component
+// public class GPrincipalExtractor implements PrincipalExtractor{
+//     @Autowired
+//     private UserService userService;
 
-    @Override
-    public Object extractPrincipal(Map<String, Object> map) {
-        String id = (String) map.get("id");
-        WMSUser user = userService.getBySub(id);
-        System.out.println("hu");
+//     @Override
+//     public Object extractPrincipal(Map<String, Object> map) {
+//         String id = (String) map.get("id");
+//         WMSUser user = userService.getBySub(id);
+//         System.out.println("hu");
 
-        if (user == null) {
-            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-            System.out.println("need to ge");
-            System.out.println(map.get("sub"));
-        } else {
-            System.out.println("got");
-            System.out.println(map.get("sub"));
-        }
-        return map.get("name");
-    }
-}
+//         if (user == null) {
+//             Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+//             System.out.println("need to ge");
+//             System.out.println(map.get("sub"));
+//         } else {
+//             System.out.println("got");
+//             System.out.println(map.get("sub"));
+//         }
+//         return map.get("name");
+//     }
+// }
