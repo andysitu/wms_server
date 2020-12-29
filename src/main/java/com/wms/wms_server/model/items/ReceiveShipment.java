5511933856c140a7a8ceeec2ba1e36a442d386b7
Andy Situ <and.situ@gmail.com>
2020-12-28 17:26:24 -0800

    Create ReceiveShipment.java

---
+++ src/main/java/com/wms/wms_server/model/items/ReceiveShipment.java
@@ -0,0 +1,35 @@
+package com.wms.wms_server.model.items;
+
+import javax.persistence.Id;
+
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import javax.persistence.EntityListeners;
+
+import javax.annotation.Generated;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+import java.util.Date;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+public class ReceiveShipment {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Integer id;
+
+    @CreatedDate
+    private Date createdDate;
+    @LastModifiedDate
+    private Date lastModifiedDate;
+    @CreatedBy
+    public String createdBy;
+    @LastModifiedBy
+    public String modifiedBy;  
+}
