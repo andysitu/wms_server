c5342084e8a5241212c9385c7365e364328a503a
Andy Situ <and.situ@gmail.com>
2020-12-29 19:38:46 -0800

    Update ItemInfoRepository with findByNameContainingIgnoreCase

--- src/main/java/com/wms/wms_server/repository/ItemInfoRepository.java
+++ src/main/java/com/wms/wms_server/repository/ItemInfoRepository.java
@@ -4,7 +4,9 @@ import com.wms.wms_server.model.items.ItemInfo;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface ItemInfoRepository extends CrudRepository<ItemInfo, Integer>{
+    List<ItemInfo> findByItemNameContainingIgnoreCase(String name);
 }
