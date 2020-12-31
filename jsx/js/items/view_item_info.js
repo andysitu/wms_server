8c014ec903417661af8f0e240aa01aa93259be93
Andy Situ <and.situ@gmail.com>
2020-12-30 19:52:44 -0800

    add itemInfo options

--- jsx/js/items/view_item_info.js
+++ jsx/js/items/view_item_info.js
@@ -25,8 +25,8 @@ class ItemInfoApp extends React.Component {
       url: "../item_info",
       type: "POST",
       data: data,
-      success: function(data) {
-        console.log(data);
+      success: function(item_data) {
+        console.log(item_data);
       },
     });
   };
@@ -35,6 +35,12 @@ class ItemInfoApp extends React.Component {
     this.modalMenu.current.show_menu(
       "create_item_info", {}, this.create_itemInfo);
   };
+
+  onClick_deleteItemInfo = (e) => {
+    console.log(e.target);
+    var result = window.confirm("Are you sure you want to delete ?")
+    console.log(result);
+  }
 
   render() {
     return (<div>
@@ -53,14 +59,31 @@ class ItemInfoApp extends React.Component {
             <th scope="col">Item Name</th>
             <th scope="col">Description</th>
             <th scope="col">Weight</th>
+            <th scope="col">Options</th>
           </tr>
         </thead>
         <tbody>
-          {this.state.itemInfos.map((itemInfo) => {
-            return (<tr>
+          {this.state.itemInfos.map((itemInfo, index) => {
+            return (<tr key={"itemInfo-" + itemInfo.id}>
               <td>{itemInfo.itemName}</td>
               <td>{itemInfo.description}</td>
               <td>{itemInfo.weight}</td>
+              <td>
+              <button type="button" className="btn btn-sm btn-outline-warning">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pencil-square" viewBox="0 0 16 16">
+                  <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456l-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+                  <path fillRule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+                </svg>
+              </button>
+              <button type="button" className="btn btn-sm btn-outline-danger"
+                onClick={this.onClick_deleteItemInfo}
+              >
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash" viewBox="0 0 16 16">
+                  <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+                  <path fillRule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+                </svg>
+              </button>
+              </td>
             </tr>);
           })}
         </tbody>
