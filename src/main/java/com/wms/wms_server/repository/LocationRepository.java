fa63feeaf6612ea37a254c139645e3aef0d67ce6
Andy Situ <and.situ@gmail.com>
2021-01-01 21:49:00 -0800

    LocationRepository extends JpaRepo & add methods for Area

--- src/main/java/com/wms/wms_server/repository/LocationRepository.java
+++ src/main/java/com/wms/wms_server/repository/LocationRepository.java
@@ -3,8 +3,16 @@ package com.wms.wms_server.repository;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.repository.CrudRepository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
 import com.wms.wms_server.model.locations.Location;
 
 @Repository
-public interface LocationRepository extends CrudRepository<Location, Integer> {
+public interface LocationRepository extends JpaRepository<Location, Integer> {
+    Page<Location> findByAreaId(Long areaId, Pageable pageable);
+    Optional<Location> findByIdAndAreaId(Integer id, Long areaId);
 }
