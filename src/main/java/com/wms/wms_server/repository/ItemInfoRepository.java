bdef9d08b02a4bcb9b4e79719b1767566202ced1
Andy Situ <and.situ@gmail.com>
2021-01-01 21:48:35 -0800

    Change ItemInfoRepository to extend from JpaRepository

--- src/main/java/com/wms/wms_server/repository/ItemInfoRepository.java
+++ src/main/java/com/wms/wms_server/repository/ItemInfoRepository.java
@@ -2,12 +2,13 @@ package com.wms.wms_server.repository;
 
 import com.wms.wms_server.model.items.ItemInfo;
 
-import org.springframework.data.repository.CrudRepository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface ItemInfoRepository extends CrudRepository<ItemInfo, Integer>{
+public interface ItemInfoRepository extends JpaRepository<ItemInfo, Integer>{
     List<ItemInfo> findByItemNameContainingIgnoreCase(String name);
     List<ItemInfo> findByDescriptionContainingIgnoreCase(String name);
 }
