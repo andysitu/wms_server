4c44e7f6585212254e7a1255a0fe7821e75fff2d
Andy Situ <and.situ@gmail.com>
2021-01-04 20:14:14 -0800

    Update LocationRepository.java

--- src/main/java/com/wms/wms_server/repository/LocationRepository.java
+++ src/main/java/com/wms/wms_server/repository/LocationRepository.java
@@ -2,8 +2,6 @@ package com.wms.wms_server.repository;
 
 import org.springframework.stereotype.Repository;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.*;
