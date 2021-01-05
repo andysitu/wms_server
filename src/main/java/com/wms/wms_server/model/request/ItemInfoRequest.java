89dcb2dc0996ace57d4248124481e9f018ea84b8
Andy Situ <and.situ@gmail.com>
2021-01-04 23:50:18 -0800

    Update ItemInfoRequest.java

--- src/main/java/com/wms/wms_server/model/request/ItemInfoRequest.java
+++ src/main/java/com/wms/wms_server/model/request/ItemInfoRequest.java
@@ -1,5 +1,10 @@
 package com.wms.wms_server.model.request;
 
 public class ItemInfoRequest {
-    
+    public String name;
+    public String description;
+    public float weight;
+    public int width;
+    public int height;
+    public int length;
 }
