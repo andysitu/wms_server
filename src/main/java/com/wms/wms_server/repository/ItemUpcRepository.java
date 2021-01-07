1015503bf6080b01189494634785fe239a741be8
Andy Situ <and.situ@gmail.com>
2021-01-06 21:38:58 -0800

    Create ItemUpcRepository.java

---
+++ src/main/java/com/wms/wms_server/repository/ItemUpcRepository.java
@@ -0,0 +1,14 @@
+package com.wms.wms_server.repository;
+
+import org.springframework.stereotype.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.*;
+
+import com.wms.wms_server.model.items.ItemUpc;
+
+@Repository
+public interface ItemUpcRepository extends JpaRepository<ItemUpc, Long> {
+    List<ItemUpc> findByItemInfoId(Long itemInfoId);
+}
