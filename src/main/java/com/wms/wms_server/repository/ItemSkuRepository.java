e18d7453101863738869aac49c29124b7e402b93
Andy Situ <and.situ@gmail.com>
2021-01-09 18:55:10 -0800

    Rename ItemUpcRepository to ItemSkuRepository

---
+++ src/main/java/com/wms/wms_server/repository/ItemSkuRepository.java
@@ -0,0 +1,15 @@
+package com.wms.wms_server.repository;
+
+import org.springframework.stereotype.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.*;
+
+import com.wms.wms_server.model.items.ItemSku;
+
+@Repository
+public interface ItemSkuRepository extends JpaRepository<ItemSku, Long> {
+    List<ItemSku> findByItemInfoId(Long itemInfoId);
+    Optional<ItemSku> findByIdAndItemInfoId(Long id, Long itemInfoId);
+}
