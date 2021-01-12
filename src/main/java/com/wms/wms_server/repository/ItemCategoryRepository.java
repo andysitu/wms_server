5ebb45fe34c524f6fe87ca99052579d9d2669ff9
Andy Situ <and.situ@gmail.com>
2021-01-11 19:54:30 -0800

    Create ItemCategoryRepository.java

---
+++ src/main/java/com/wms/wms_server/repository/ItemCategoryRepository.java
@@ -0,0 +1,13 @@
+package com.wms.wms_server.repository;
+
+import com.wms.wms_server.model.items.ItemCategory;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ItemCategoryRepository extends JpaRepository<ItemCategory, Long> {
+    
+}
