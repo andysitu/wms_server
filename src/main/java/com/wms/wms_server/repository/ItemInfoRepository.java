98046b6125679b4eef7f613480c965ce9ce65234
Andy Situ <and.situ@gmail.com>
2021-01-13 19:32:57 -0800

    Add manual query in ItemInfoRepo: findByItemCategoryName

--- src/main/java/com/wms/wms_server/repository/ItemInfoRepository.java
+++ src/main/java/com/wms/wms_server/repository/ItemInfoRepository.java
@@ -3,6 +3,7 @@ package com.wms.wms_server.repository;
 import com.wms.wms_server.model.items.ItemInfo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
@@ -10,4 +11,7 @@ import java.util.List;
 public interface ItemInfoRepository extends JpaRepository<ItemInfo, Long>{
     List<ItemInfo> findByItemNameContainingIgnoreCase(String name);
     List<ItemInfo> findByDescriptionContainingIgnoreCase(String name);
+
+    @Query("SELECT i FROM ItemInfo i inner join i.itemCategory c where c.name = :name")
+    List<ItemInfo> findByItemCategoryName(String name);
 }
