9430e4e71a1b931ec2389f732e6b2b67695d0bc5
Andy Situ <and.situ@gmail.com>
2021-01-13 20:41:40 -0800

    Create ItemInventory.java

---
+++ src/main/java/com/wms/wms_server/model/items/ItemInventory.java
@@ -0,0 +1,20 @@
+package com.wms.wms_server.model.items;
+
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+public class ItemInventory {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
+
+    private int quantity;
+}
+
