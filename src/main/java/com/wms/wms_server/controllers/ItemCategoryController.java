dc4bc6f88c09b87affd174d375ba76dada3ed0d7
Andy Situ <and.situ@gmail.com>
2021-01-15 12:59:22 -0800

    Add get_categories to the categories controller

--- src/main/java/com/wms/wms_server/controllers/ItemCategoryController.java
+++ src/main/java/com/wms/wms_server/controllers/ItemCategoryController.java
@@ -1,11 +1,18 @@
 package com.wms.wms_server.controllers;
 
+import java.util.ArrayList;
+
+import com.wms.wms_server.model.items.ItemCategory;
+import com.wms.wms_server.model.response.ItemCategoryResponse;
 import com.wms.wms_server.repository.ItemCategoryRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.*;
 
 
 @Controller
@@ -14,8 +21,20 @@ public class ItemCategoryController {
     ItemCategoryRepository itemCategoryRepository;
 
     @GetMapping(value="/view_categories")
-    public String getMethodName() {
+    public String view_categories() {
         return "category/view_categories";
     }
     
+    @GetMapping(value="/categories")
+    @ResponseBody
+    public List<ItemCategoryResponse> get_categories() {
+        List<ItemCategoryResponse> responses = new ArrayList<>();
+        for (ItemCategory category : itemCategoryRepository.findAll()) {
+            responses.add(
+                new ItemCategoryResponse(category.getId(), category.getName(), 
+                category.getCreatedDate(), category.getLastModifiedDate())
+            );
+        };
+        return responses;
+    }
 }
