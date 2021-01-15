9d8f8edba53264c8f592e4fd4edff144b6bf5f30
Andy Situ <and.situ@gmail.com>
2021-01-14 23:23:19 -0800

    Create Warehouse.java

---
+++ src/main/java/com/wms/wms_server/model/locations/Warehouse.java
@@ -0,0 +1,40 @@
+package com.wms.wms_server.model.locations;
+
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import java.util.Date;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+public class Warehouse {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Integer id;
+
+    @CreatedDate
+    private Date createdDate;
+    @LastModifiedDate
+    private Date lastModifiedDate;
+    @CreatedBy
+    private String createdBy;
+    @LastModifiedBy
+    private String modifiedBy;
+
+    private String address_1;
+    private String address_2;
+    private String city;
+    private String state;
+    private String country;
+    private String zip;
+}
