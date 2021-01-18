dc70094f9c79945d65c355a2bd2558a1e00a4e26
Andy Situ <and.situ@gmail.com>
2021-01-17 21:44:25 -0800

    Create warehouse via @Valid & @RequestBody directly

--- src/main/java/com/wms/wms_server/controllers/WarehouseController.java
+++ src/main/java/com/wms/wms_server/controllers/WarehouseController.java
@@ -5,9 +5,13 @@ import com.wms.wms_server.repository.WarehouseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.wms.wms_server.model.locations.Warehouse;
+
+import javax.validation.Valid;
 
 import java.util.*;
 
@@ -26,5 +30,11 @@ public class WarehouseController {
     public List<Warehouse> get_warehouses() {
         List<Warehouse> warehouses = warehouseRepository.findAll();
         return warehouses;
+    }
+
+    @PostMapping(value="/warehouses", consumes = "application/json")
+    @ResponseBody
+    public Warehouse create_warehouse(@Valid @RequestBody Warehouse warehouse) {
+        return warehouseRepository.save(warehouse);
     }
 }
