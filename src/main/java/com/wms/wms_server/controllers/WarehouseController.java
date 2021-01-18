4e40c487143a863adba9f9e22c9b7dd6715daeef
Andy Situ <and.situ@gmail.com>
2021-01-17 20:37:56 -0800

    Add get_warehouses to WarehouseController

--- src/main/java/com/wms/wms_server/controllers/WarehouseController.java
+++ src/main/java/com/wms/wms_server/controllers/WarehouseController.java
@@ -1,12 +1,28 @@
 package com.wms.wms_server.controllers;
 
+import com.wms.wms_server.repository.WarehouseRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.wms.wms_server.model.locations.Warehouse;
+
+import java.util.*;
+
 @Controller
 public class WarehouseController {
+    @Autowired
+    WarehouseRepository warehouseRepository;
+    
     @GetMapping(value = "/view_warehouses")
     public String view_warehouses() {
         return "locations/view_warehouses";
+    }
+
+    @GetMapping(value="/warehouses")
+    public List<Warehouse> get_warehouses() {
+        List<Warehouse> warehouses = warehouseRepository.findAll();
+        return warehouses;
     }
 }
