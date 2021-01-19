239b12ad3bd53f6da705d71b6945ace9ff9151b3
Andy Situ <and.situ@gmail.com>
2021-01-18 16:27:45 -0800

    WarehouseController.get_warehouse() returns warehouseResponse

--- src/main/java/com/wms/wms_server/controllers/WarehouseController.java
+++ src/main/java/com/wms/wms_server/controllers/WarehouseController.java
@@ -1,6 +1,7 @@
 package com.wms.wms_server.controllers;
 
 import com.wms.wms_server.repository.WarehouseRepository;
+import com.wms.wms_server.services.WarehouseService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.wms.wms_server.model.locations.Warehouse;
+import com.wms.wms_server.model.response.WarehouseResponse;
 
 import javax.validation.Valid;
 
@@ -19,6 +21,8 @@ import java.util.*;
 public class WarehouseController {
     @Autowired
     WarehouseRepository warehouseRepository;
+    @Autowired
+    WarehouseService warehouseService;
     
     @GetMapping(value = "/view_warehouses")
     public String view_warehouses() {
@@ -27,9 +31,12 @@ public class WarehouseController {
 
     @GetMapping(value="/warehouses")
     @ResponseBody
-    public List<Warehouse> get_warehouses() {
-        List<Warehouse> warehouses = warehouseRepository.findAll();
-        return warehouses;
+    public List<WarehouseResponse> get_warehouses() {
+        ArrayList<WarehouseResponse> responses = new ArrayList<>();
+        for (Warehouse warehouse : warehouseRepository.findAll()) {
+            responses.add(warehouseService.convert_to_response(warehouse));
+        }
+        return responses;
     }
 
     @PostMapping(value="/warehouses", consumes = "application/json")
