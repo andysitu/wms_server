014c622ef1301c54cbea6856e19d8de05d4393a8
Andy Situ <and.situ@gmail.com>
2021-01-18 16:27:16 -0800

    Create WarehouseResponse.java

---
+++ src/main/java/com/wms/wms_server/model/response/WarehouseResponse.java
@@ -0,0 +1,21 @@
+package com.wms.wms_server.model.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class WarehouseResponse {
+    @Getter @Setter private int id;    
+    @Getter @Setter private String name;
+    @Getter @Setter private String description;
+    @Getter @Setter private String address1;
+    @Getter @Setter private String address2;
+    @Getter @Setter private String city;
+    @Getter @Setter private String state;
+    @Getter @Setter private String country;
+    @Getter @Setter private String zip;
+    @Getter @Setter private String phone;
+
+    public WarehouseResponse(String name) {
+        this.name = name;
+    }
+}
