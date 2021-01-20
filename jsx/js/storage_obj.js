4eff594aa830ca6442a697cae3c57e1bd235fce5
Andy Situ <and.situ@gmail.com>
2021-01-19 18:33:05 -0800

    Create storage_obj to save data to localStorage

---
+++ jsx/js/storage_obj.js
@@ -0,0 +1,15 @@
+var storage_obj = {
+  warehouse_property: "default_warehouse_id",
+  get_warehouse() {
+    return window.locatStorage.getItem(this.warehouse_property);
+  },
+  set_warehouse(warehouse_id) {
+    window.localStorage.setitem(this.warehouse_property, warehouse_id);
+  },
+  clear_warehouse_if_id(warehouse_id) {
+    var w_id = this.get_warehouse();
+    if (warehouse_id == w_id) {
+      window.localStorage.removeItem(this.warehouse_property);
+    }
+  },
+}
