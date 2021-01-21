f376b03201f3a442353c52860470da931acc6778
Andy Situ <and.situ@gmail.com>
2021-01-21 15:22:58 -0800

    Add findByWarehouseId to AreaRepository

--- src/main/java/com/wms/wms_server/repository/AreaRepository.java
+++ src/main/java/com/wms/wms_server/repository/AreaRepository.java
@@ -5,8 +5,12 @@ import com.wms.wms_server.model.locations.Area;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AreaRepository extends JpaRepository<Area, Long>{
     public Boolean existsByArea(String area);
     public Area getByArea(String area);
+    public List<Area> findByWarehouseId(Long warehouseId);
 }
+ 
