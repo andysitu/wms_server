0cd22e2657711634733cb4c17713a4f23965b599
Andy Situ <and.situ@gmail.com>
2021-01-23 17:52:54 -0800

    Update ItemInfo.java

--- src/main/java/com/wms/wms_server/model/items/ItemInfo.java
+++ src/main/java/com/wms/wms_server/model/items/ItemInfo.java
@@ -3,7 +3,6 @@ package com.wms.wms_server.model.items;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.FetchType;
 
 import org.springframework.data.annotation.LastModifiedBy;
@@ -16,14 +15,12 @@ import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import javax.persistence.EntityListeners;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 
 import java.util.Date;
-import java.util.Set;
 
 @Entity
 @EntityListeners(AuditingEntityListener.class)
