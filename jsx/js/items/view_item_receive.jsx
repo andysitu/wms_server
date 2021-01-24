e2ee267e083e96de55f8505ecf1e9205a87ba52a
Andy Situ <and.situ@gmail.com>
2021-01-23 17:27:10 -0800

    Add blank view_item_receive page

---
+++ jsx/js/items/view_item_receive.jsx
@@ -0,0 +1,25 @@
+class ItemReceiveApp extends React.Component {
+  render() {
+    return (<div>
+      <table>
+        <thead>
+          <tr>
+            <th scole="col">Item Name</th>
+            <th scole="col">Shipment</th>
+            <th scole="col">Quantity</th>
+            <th scole="col">Date</th>
+            <th scole="col">Options</th>
+          </tr>
+        </thead>
+      </table>
+    </div>);
+  }
+}
+
+function loadReact() {
+  ReactDOM.render((
+    <ItemReceiveApp />
+  ), document.getElementById("content-container"));
+}
+
+loadReact();
---
+++ src/main/resources/static/js/items/view_item_receive.js
@@ -0,0 +1,72 @@
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var ItemReceiveApp = function (_React$Component) {
+  _inherits(ItemReceiveApp, _React$Component);
+
+  function ItemReceiveApp() {
+    _classCallCheck(this, ItemReceiveApp);
+
+    return _possibleConstructorReturn(this, (ItemReceiveApp.__proto__ || Object.getPrototypeOf(ItemReceiveApp)).apply(this, arguments));
+  }
+
+  _createClass(ItemReceiveApp, [{
+    key: "render",
+    value: function render() {
+      return React.createElement(
+        "div",
+        null,
+        React.createElement(
+          "table",
+          null,
+          React.createElement(
+            "thead",
+            null,
+            React.createElement(
+              "tr",
+              null,
+              React.createElement(
+                "th",
+                { scole: "col" },
+                "Item Name"
+              ),
+              React.createElement(
+                "th",
+                { scole: "col" },
+                "Shipment"
+              ),
+              React.createElement(
+                "th",
+                { scole: "col" },
+                "Quantity"
+              ),
+              React.createElement(
+                "th",
+                { scole: "col" },
+                "Date"
+              ),
+              React.createElement(
+                "th",
+                { scole: "col" },
+                "Options"
+              )
+            )
+          )
+        )
+      );
+    }
+  }]);
+
+  return ItemReceiveApp;
+}(React.Component);
+
+function loadReact() {
+  ReactDOM.render(React.createElement(ItemReceiveApp, null), document.getElementById("content-container"));
+}
+
+loadReact();
