7361588a2eac383230a7e71d8034e93d936a436c
Andy Situ <and.situ@gmail.com>
2021-01-24 17:37:06 -0800

    Update ShipmentReceiveRepository.java

--- src/main/java/com/wms/wms_server/repository/ShipmentReceiveRepository.java
+++ src/main/java/com/wms/wms_server/repository/ShipmentReceiveRepository.java
@@ -1,11 +1,11 @@
 package com.wms.wms_server.repository;
 
-import com.wms.wms_server.model.items.ShipmenetReceive;
+import com.wms.wms_server.model.shipments.ShipmentReceive;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ShipmentReceiveRepository extends JpaRepository<ShipmenetReceive, Long> {
+public interface ShipmentReceiveRepository extends JpaRepository<ShipmentReceive, Long> {
     
 }
