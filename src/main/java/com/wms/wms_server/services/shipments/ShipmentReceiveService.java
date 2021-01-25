e0478973c4373e06a0bb33efe59143c48e84ef6d
Andy Situ <and.situ@gmail.com>
2021-01-24 17:57:21 -0800

    Create ShipmentReceiveService.java

---
+++ src/main/java/com/wms/wms_server/services/shipments/ShipmentReceiveService.java
@@ -0,0 +1,26 @@
+package com.wms.wms_server.services.shipments;
+
+import com.wms.wms_server.model.shipments.ShipmentReceive;
+import com.wms.wms_server.repository.ShipmentReceiveRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+@Service
+public class ShipmentReceiveService {
+    @Autowired
+    ShipmentReceiveRepository shipmentReceiveRepository;
+
+    public ShipmentReceive get_or_create_shipmentReceive(String shipmentCode) {
+        ArrayList<ShipmentReceive> shipments = shipmentReceiveRepository.findByCode(shipmentCode);
+        if (shipments.size() == 0) {
+            return shipments.get(0);
+        } else {
+            ShipmentReceive shipment = new ShipmentReceive(shipmentCode);
+            shipmentReceiveRepository.save(shipment);
+            return shipment;
+        }
+    }
+}
