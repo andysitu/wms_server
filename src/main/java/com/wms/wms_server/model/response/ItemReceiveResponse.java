35457f2aed9ae1fd95e759a8a0c11d945ae2a68b
Andy Situ <and.situ@gmail.com>
2021-01-24 20:41:55 -0800

    Create ItemReceiveResponse.java

---
+++ src/main/java/com/wms/wms_server/model/response/ItemReceiveResponse.java
@@ -0,0 +1,8 @@
+package com.wms.wms_server.model.response;
+
+public class ItemReceiveResponse {
+    public Long id;
+    public int quantity;
+    public String itemSku;
+    public String shipmentCode;
+}
