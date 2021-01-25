3a0f12e2bd6f201f0cb71d5f66556bd6e2a1a929
Andy Situ <and.situ@gmail.com>
2021-01-24 20:21:49 -0800

    Add constructor for ItemReceive & change shipment->shipmentReceive

--- src/main/java/com/wms/wms_server/model/items/ItemReceive.java
+++ src/main/java/com/wms/wms_server/model/items/ItemReceive.java
@@ -31,10 +31,18 @@ public class ItemReceive {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name="shipment_receive_id", nullable = false)
     @OnDelete(action = OnDeleteAction.CASCADE)
-    @Getter @Setter private ShipmentReceive shipment;
+    @Getter @Setter private ShipmentReceive shipmentReceive;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name="item_info_id", nullable = false)
     @OnDelete(action = OnDeleteAction.CASCADE)
     @Getter @Setter private ItemInfo itemInfo;
+
+
+    public ItemReceive(int quantity, ShipmentReceive shipmentReceive, 
+            ItemInfo itemInfo) {
+        this.quantity = quantity;
+        this.shipmentReceive = shipmentReceive;
+        this.itemInfo = itemInfo;
+    }
 }
