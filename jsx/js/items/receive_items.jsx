b14823a6448a98fcc3af79db591ddaad5d0fc537
Andy Situ <and.situ@gmail.com>
2021-01-26 18:08:00 -0800

    Add deleting of items in receiving items page

--- jsx/js/items/receive_items.jsx
+++ jsx/js/items/receive_items.jsx
@@ -57,6 +57,30 @@ class ReceiveItemApp extends React.Component {
     });
   };
 
+  // Deletes item from state whether there was an error in request or not
+  onClick_delete_item = (e) => {
+    var item_id = e.target.getAttribute("item_id"),
+        index = e.target.getAttribute("index");
+    if (item_id) {
+      $.ajax({
+        url: "./itemreceive/" + e.target.getAttribute("item_id"),
+        type: "DELETE",
+        context: this,
+        complete: function() {
+          this.setState(state => {
+            var new_items = [...state.received_items];
+            new_items.splice(index, 1);
+            return {
+              received_items: new_items,
+            };
+          });
+        }
+      });
+    }
+    
+
+  }
+
   render() {
     return (<div>
       <form onSubmit={this.onSubmit_item}>
@@ -88,14 +112,23 @@ class ReceiveItemApp extends React.Component {
           </tr>
         </thead>
         <tbody>
-          {this.state.received_items.map((item)=> {
+          {this.state.received_items.map((item, index)=> {
             return (
               <tr key={item.id}>
                 <td>{item.itemInfoResponse.itemName}</td>
                 <td>{item.itemSku}</td>
                 <td>{item.shipmentCode}</td>
                 <td>{item.quantity}</td>
-                <td></td>
+                <td>
+                <button type="button" className="btn btn-sm btn-outline-danger"
+                  item_id={item.id} index={index} title="Delete item"
+                  onClick={this.onClick_delete_item}>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash" viewBox="0 0 16 16">
+                    <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+                    <path fillRule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+                  </svg>
+                </button>
+                </td>
               </tr>);
           })}
         </tbody>
