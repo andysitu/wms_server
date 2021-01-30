690a90218fa60fcdb1fc89a078b3b2a6fa0a4a2a
Andy Situ <and.situ@gmail.com>
2021-01-29 16:44:13 -0800

    Add ItemReceive table to putaway page

--- jsx/js/items/putaway_items.jsx
+++ jsx/js/items/putaway_items.jsx
@@ -7,16 +7,33 @@ class PutawayApp extends React.Component {
     return (
     <div>
       <form onSubmit={this.onSubmit_searchForm}>
-        <div className="form-group">
-          <label htmlFor="">Shipment Code</label>
-          <input type="text" className="form-control" name="shipmentCode"></input>
-        </div>
-        <div className="form-group">
-          <label htmlFor="">Item SKU</label>
-          <input type="text" className="form-control" name="itemSku"></input>
+        <div className="form-group row">
+          <div className="col-5">
+            <label htmlFor="">Shipment Code</label>
+            <input type="text" className="form-control" name="shipmentCode"></input>
+          </div>
+          <div className="col-5">
+            <label htmlFor="">Item SKU</label>
+            <input type="text" className="form-control" name="itemSku"></input>
+          </div>  
         </div>
         <button type="submit">Search</button>
       </form>
+
+      <div>
+        <table className="table table-sm">
+          <thead>
+            <tr>
+              <th scope="col">Shipment Code</th>
+              <th scope="col">Item Name</th>
+              <th scope="col">SKU</th>
+              <th scope="col">Quantity</th>
+              <th scope="col">Quantity</th>
+            </tr>
+          </thead>
+          <tbody id="itemReceive-table"></tbody>
+        </table>
+      </div>
     </div>);
   }
 }
