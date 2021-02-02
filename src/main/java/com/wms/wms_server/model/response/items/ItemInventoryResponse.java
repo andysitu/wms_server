c060a3a9f7a063d35ad8ff8c93c5302ab31f2d1f
Andy Situ <and.situ@gmail.com>
2021-02-01 18:49:28 -0800

    Create ItemInventoryResponse.java

---
+++ src/main/java/com/wms/wms_server/model/response/items/ItemInventoryResponse.java
@@ -0,0 +1,10 @@
+package com.wms.wms_server.model.response.items;
+
+public class ItemInventoryResponse {
+    public int quantity;
+    public long Id;
+    public String itemName;
+    public String locationCode;
+    public String shipmentCode;
+    public String itemSku;
+}
