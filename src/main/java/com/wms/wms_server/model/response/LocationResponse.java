843738317d289963f1e8be697eb59cf95422981b
Andy Situ <and.situ@gmail.com>
2021-02-01 17:23:05 -0800

    Add locationCode to LocationResponse

--- src/main/java/com/wms/wms_server/model/response/LocationResponse.java
+++ src/main/java/com/wms/wms_server/model/response/LocationResponse.java
@@ -8,13 +8,16 @@ public class LocationResponse {
     public int level;
     public int shelf;
     public Long area_id;
+    public String locationCode;
 
-    public LocationResponse(Integer id, String area, int row, int bay, int level, int shelf) {
+    public LocationResponse(Integer id, String area, int row, 
+        int bay, int level, int shelf, String locationCode) {
         this.id = id;
         this.area = area;
         this.row = row;
         this.bay = bay;
         this.level = level;
         this.shelf = shelf;
+        this.locationCode = locationCode;
     }
 }
