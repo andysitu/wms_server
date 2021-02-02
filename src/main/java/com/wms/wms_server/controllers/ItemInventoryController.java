3590ef7e98878cb0a8c747df50fec98aeb91aaf8
Andy Situ <and.situ@gmail.com>
2021-02-01 16:52:33 -0800

    Minor update in inventory service & controller

--- src/main/java/com/wms/wms_server/controllers/ItemInventoryController.java
+++ src/main/java/com/wms/wms_server/controllers/ItemInventoryController.java
@@ -1,14 +1,33 @@
 package com.wms.wms_server.controllers;
 
+import javax.servlet.http.HttpServletRequest;
+
+import com.wms.wms_server.services.items.ItemInventoryService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 
 @Controller
 public class ItemInventoryController {
+    @Autowired
+    ItemInventoryService itemInventoryService;
+
     @GetMapping("/view_item_putaway")
     public String view_item_putaway() {
         return "items/view_item_putaway";
     }
+
+    @RequestMapping(value="/iteminventory", method=RequestMethod.POST)
+    @ResponseBody
+    public String createItemInventory(HttpServletRequest request) {
+
+        return "OK";
+    }
+    
 }
--- src/main/java/com/wms/wms_server/services/items/ItemInventoryService.java
+++ src/main/java/com/wms/wms_server/services/items/ItemInventoryService.java
@@ -2,13 +2,18 @@ package com.wms.wms_server.services.items;
 
 import javax.servlet.http.HttpServletRequest;
 
+import com.wms.wms_server.repository.LocationRepository;
 import com.wms.wms_server.repository.items.ItemInventoryRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ItemInventoryService {
     @Autowired
     private ItemInventoryRepository itemInventoryRepository;
+    @Autowired
+    private LocationRepository locationRepository;
     
     public Boolean check_request(HttpServletRequest request, String parameter) {
         String result = request.getParameter(parameter);
