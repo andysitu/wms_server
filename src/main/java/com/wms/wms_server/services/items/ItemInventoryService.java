635e6c98496f11a4d89f3be6d57a5538288fe221
Andy Situ <and.situ@gmail.com>
2021-02-01 16:02:33 -0800

    Create ItemInventoryService.java

---
+++ src/main/java/com/wms/wms_server/services/items/ItemInventoryService.java
@@ -0,0 +1,19 @@
+package com.wms.wms_server.services.items;
+
+import javax.servlet.http.HttpServletRequest;
+
+import com.wms.wms_server.repository.items.ItemInventoryRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class ItemInventoryService {
+    @Autowired
+    private ItemInventoryRepository itemInventoryRepository;
+    
+    public Boolean check_request(HttpServletRequest request, String parameter) {
+        String result = request.getParameter(parameter);
+        return (result != null && result.length() > 0);
+    }
+
+    
+}
