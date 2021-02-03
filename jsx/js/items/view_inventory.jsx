103099adf3a08cd579d1056f62d1e11742332aff
Andy Situ <and.situ@gmail.com>
2021-02-02 19:20:16 -0800

    Update view_inventory.jsx

--- jsx/js/items/view_inventory.jsx
+++ jsx/js/items/view_inventory.jsx
@@ -1,1 +1,27 @@
-console.log("Hello");
+class InventoryApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.loadItemInventory();
+  }
+
+  loadItemInventory = () => {
+    $.ajax({
+      url: "./iteminventory",
+      type: "GET",
+      context: this,
+      success: function(data) {
+        console.log(data);
+      }
+    });
+  };
+
+  render() {
+    return (<div>HI</div>);
+  }
+}
+
+function loadReact() {
+  ReactDOM.render((<InventoryApp />), document.getElementById("content-container"));
+}
+
+loadReact();
