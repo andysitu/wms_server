3656878d0e2231f5516c02008aa5cc0d88b52bc4
Andy Situ <and.situ@gmail.com>
2021-02-02 19:30:38 -0800

    Update view_inventory.jsx

--- jsx/js/items/view_inventory.jsx
+++ jsx/js/items/view_inventory.jsx
@@ -1,6 +1,10 @@
 class InventoryApp extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      itemInventory: [],
+    }
+
     this.loadItemInventory();
   }
 
@@ -9,14 +13,41 @@ class InventoryApp extends React.Component {
       url: "./iteminventory",
       type: "GET",
       context: this,
-      success: function(data) {
-        console.log(data);
+      success: function(items) {
+        console.log(items);
+        this.setState({
+          itemInventory: items,
+        });
       }
     });
   };
 
   render() {
-    return (<div>HI</div>);
+    return (<div>
+      <table className="table table-sm">
+        <thead>
+          <tr>
+            <th scope="col">Item Name</th>
+            <th scope="col">SKU</th>
+            <th scope="col">Quantity</th>
+            <th scope="col">Shipment Code</th>
+            <th scope="col">Location</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.state.itemInventory.map((item, index) => {
+            return (
+            <tr key={item.id}>
+              <td>{item.itemName}</td>
+              <td>{item.itemSku}</td>
+              <td>{item.quantity}</td>
+              <td>{item.shipmentCode}</td>
+              <td>{item.locationCode}</td>
+            </tr>);
+          })}
+        </tbody>
+      </table>
+    </div>);
   }
 }
 
