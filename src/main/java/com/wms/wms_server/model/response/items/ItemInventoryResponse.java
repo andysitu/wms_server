c9d7f8adfa4bef8079e81c81da23be4cccfc04a3
Andy Situ <and.situ@gmail.com>
2021-02-02 18:38:06 -0800

    Update ItemInventoryResponse.java

--- src/main/java/com/wms/wms_server/model/response/items/ItemInventoryResponse.java
+++ src/main/java/com/wms/wms_server/model/response/items/ItemInventoryResponse.java
@@ -2,7 +2,7 @@ package com.wms.wms_server.model.response.items;
 
 public class ItemInventoryResponse {
     public int quantity;
-    public long Id;
+    public long id;
     public String itemName;
     public String locationCode;
     public String shipmentCode;
