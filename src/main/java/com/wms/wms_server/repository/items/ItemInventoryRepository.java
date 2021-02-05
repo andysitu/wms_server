06b2846fe4a9e9195109c1e852431db04536b75a
Andy Situ <and.situ@gmail.com>
2021-02-04 16:56:28 -0800

    Add findByItemName to ItemInventoryRepository

--- src/main/java/com/wms/wms_server/repository/items/ItemInventoryRepository.java
+++ src/main/java/com/wms/wms_server/repository/items/ItemInventoryRepository.java
@@ -3,9 +3,13 @@ package com.wms.wms_server.repository.items;
 import com.wms.wms_server.model.items.ItemInventory;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.*;
 
 @Repository
 public interface ItemInventoryRepository extends JpaRepository<ItemInventory, Long> {
-    
+    @Query("Select i from ItemInventory i INNER JOIN i.itemInfo iinfo WHERE LOWER(iinfo.itemName) LIKE LOWER(CONCAT('%',:name,'%'))")
+    List<ItemInventory> findByItemName(String name);
 }
