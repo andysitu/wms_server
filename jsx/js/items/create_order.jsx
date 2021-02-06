7487a6f6d3555b6312f40f58de3778ef65e2b250
Andy Situ <and.situ@gmail.com>
2021-02-05 16:55:07 -0800

    Create order usese InventoryTableOrder

--- jsx/js/items/create_order.jsx
+++ jsx/js/items/create_order.jsx
@@ -1,9 +1,9 @@
-import { InventoryTable } from "./InventoryTable.js"
+import { InventoryTableOrder } from "./InventoryTableOrder.js"
 
 class CreateOrderApp extends React.Component {
   render() {
     return (<div>
-      <InventoryTable type={"ship_items"}/>
+      <InventoryTableOrder type={"ship_items"}/>
     </div>);
   }
 }
