32cbebcddb9174851996a7fcabb27960e1b9d53e
Andy Situ <and.situ@gmail.com>
2021-02-06 18:55:21 -0800

    Create CreateLocationMenu.js

--- jsx/js/etc/modalmenu/ModalMenu.jsx
+++ jsx/js/etc/modalmenu/ModalMenu.jsx
@@ -1,3 +1,5 @@
+import { CreateLocationMenu } from "./CreateLocationMenu.js"
+
 export {
   ModalMenu,
 }
@@ -453,59 +455,6 @@ class ModalMenu extends React.Component {
           </form>
         </div>
       </div>
-    </div>);
-  }
-}
-
-class CreateLocationMenu extends React.Component {
-  render() {
-    return (<div>
-      <h3>Warehouse: {this.props.warehouse_name}-{this.props.warehouse_code}</h3>
-      <div className="form-group">
-        <label>Area</label>
-        <input type="text" className="form-control" min="0" name="area" required></input>
-      </div>
-      <div className="form-row">
-        <div className="form-group col-sm-6">
-          <label>Start Row</label>
-          <input type="number" className="form-control" min="0" name="row_start" required></input>
-        </div>
-        <div className="form-group col-sm-6">
-          <label>End Row</label>
-          <input type="number" className="form-control" min="0" name="row_end"></input>
-        </div>
-      </div>
-      <div className="form-row">
-        <div className="form-group col-sm-6">
-          <label>Start Bay</label>
-          <input type="number" className="form-control" min="0" name="bay_start" required></input>
-        </div>
-        <div className="form-group col-sm-6">
-          <label>End Bay</label>
-          <input type="number" className="form-control" min="0" name="bay_end"></input>
-        </div>
-      </div>
-
-      <div className="form-row">
-        <div className="form-group col-sm-6">
-          <label>Start Level</label>
-          <input type="number" className="form-control" min="0" name="level_start" required></input>
-        </div>
-        <div className="form-group col-sm-6">
-          <label>End Level</label>
-          <input type="number" className="form-control" min="0" name="level_end"></input>
-        </div>
-      </div>
-      <div className="form-row">
-        <div className="form-group col-sm-6">
-          <label>Start Shelf</label>
-          <input type="number" className="form-control" min="0" name="shelf_start" required></input>
-        </div>
-        <div className="form-group col-sm-6">
-          <label>End Shelf</label>
-          <input type="number" className="form-control" min="0" name="shelf_end"></input>
-        </div>
-      </div>                                                                                                                                                                                                                                                                                                                          
     </div>);
   }
 }
