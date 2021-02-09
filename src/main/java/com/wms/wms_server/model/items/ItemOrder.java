eda7f3804e15a3c17fb769a17d1122fc6981d8b7
Andy Situ <and.situ@gmail.com>
2021-02-08 19:39:12 -0800

    Create ItemOrder.java

---
+++ src/main/java/com/wms/wms_server/model/items/ItemOrder.java
@@ -0,0 +1,48 @@
+package com.wms.wms_server.model.items;
+
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import javax.persistence.EntityListeners;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.FetchType;
+
+import java.util.Date;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+public class ItemOrder {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    @Getter private Long id;
+
+    @CreatedDate
+    private Date createdDate;
+    @LastModifiedDate
+    private Date lastModifiedDate;
+    @CreatedBy
+    private String createdBy;
+    @LastModifiedBy
+    private String modifiedBy;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(nullable = false)
+    private ItemInventory itemInventory;
+
+    private int orderedQuantity;
+    private int quantity;
+}
