4be153a12d54bfea1d01b11c506309fe28005d0b
Andy Situ <and.situ@gmail.com>
2021-02-08 20:25:46 -0800

    Delete ItemOrderService.java

--- src/main/java/com/wms/wms_server/services/items/ItemOrderService.java
+++
@@ -1,9 +0,0 @@
-package com.wms.wms_server.services.items;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-@Service
-public class ItemOrderService {
-    // @Autowired
-}
