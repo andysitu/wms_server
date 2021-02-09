47ee4ce4e837ae358d5b6e20dfbd96136cad4b43
Andy Situ <and.situ@gmail.com>
2021-02-08 19:39:16 -0800

    Create ItemOrderService.java

---
+++ src/main/java/com/wms/wms_server/services/items/ItemOrderService.java
@@ -0,0 +1,9 @@
+package com.wms.wms_server.services.items;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ItemOrderService {
+    // @Autowired
+}
