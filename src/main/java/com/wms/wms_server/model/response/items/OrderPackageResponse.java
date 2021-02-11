934378e08c52a588fa1ab208b8a596dc01f9861f
Andy Situ <and.situ@gmail.com>
2021-02-10 19:10:49 -0800

    Create OrderPackageResponse.java

---
+++ src/main/java/com/wms/wms_server/model/response/items/OrderPackageResponse.java
@@ -0,0 +1,19 @@
+package com.wms.wms_server.model.response.items;
+
+public class OrderPackageResponse {
+    public long id;
+    public String orderName;
+    public String description;
+
+    public String contactName;
+    public String companyName;
+    public String address1;
+    public String address2;
+    public String city;
+    public String state;
+    public String zip;
+
+    public String transportName;
+
+    public int complete;
+}
