2a4c3f0e0351f8e52d8a0bcba6adac1af84417dd
Andy Situ <and.situ@gmail.com>
2021-02-11 17:33:11 -0800

    Create ItemOrderService with convertToResponse()

---
+++ src/main/java/com/wms/wms_server/services/items/ItemOrderService.java
@@ -0,0 +1,24 @@
+package com.wms.wms_server.services.items;
+
+import com.wms.wms_server.model.items.ItemOrder;
+import com.wms.wms_server.model.response.items.ItemOrderResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ItemOrderService {
+    @Autowired ItemInventoryService itemInventoryService;
+
+    public ItemOrderResponse convertToResponse(ItemOrder itemOrder) {
+        ItemOrderResponse response = new ItemOrderResponse();
+        response.id = itemOrder.getId();
+        response.orderedQuantity = itemOrder.getOrderedQuantity();
+        response.startQuantity = itemOrder.getStartQuantity();
+        response.pickedQuantity = itemOrder.getPickedQuantity();
+        response.complete = itemOrder.getComplete();
+        response.itemInventoryReseponse = itemInventoryService.convert_to_response(
+            itemOrder.getItemInventory());
+        return response;
+    }
+}
