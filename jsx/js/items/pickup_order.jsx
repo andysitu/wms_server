9932987931888ebd0b72269458e6e3d17bd6e6d6
Andy Situ <and.situ@gmail.com>
2021-02-11 17:47:28 -0800

    Show orders in pickup order page

--- jsx/js/items/pickup_order.jsx
+++ jsx/js/items/pickup_order.jsx
@@ -1,6 +1,9 @@
 class PickupOrderApp extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      orders: [],
+    }
     this.getOrders();
   }
 
@@ -11,15 +14,49 @@ class PickupOrderApp extends React.Component {
       context: this,
       success: function(orders) {
         console.log(orders);
+        this.setState({
+          orders: orders,
+        });
       }
-    })
+    });
   };
 
   render() {
+    let numOpenItems, i;
     return (
     <div>
       <h2>Open Orders</h2>
       <div>
+        <table>
+          <thead>
+            <tr>
+              <th scope="col">Order Name</th>
+              <th scope="col">Company Name</th>
+              <th scope="col">Transport</th>
+              <th scope="col">Open Item Rows</th>
+              <th scope="col">Total Item Row</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.orders.map(((orderPackage, index) => {
+              numOpenItems = 0;
+              for (i=0; i<orderPackage.itemOrderResponses.length; i++) {
+                if (orderPackage.itemOrderResponses[i].complete == 0) {
+                  numOpenItems++;
+                }
+              }
+              return (
+                <tr key={orderPackage.id}>
+                  <td>{orderPackage.orderName}</td>
+                  <td>{orderPackage.companyName}</td>
+                  <td>{orderPackage.transportName}</td>
+                  <td>{numOpenItems}</td>
+                  <td>{orderPackage.itemOrderResponses.length}</td>
+                </tr>)
+            }))}
+          </tbody>
+        </table>
+        
       </div>
     </div>      
     );
