e0665d7fcc7d2814b69b542a3244dadac9b4f08b
Andy Situ <and.situ@gmail.com>
2021-02-11 18:02:48 -0800

    Add findByComplete to OrderPackageRepsoitory

--- src/main/java/com/wms/wms_server/repository/items/OrderPackageRepository.java
+++ src/main/java/com/wms/wms_server/repository/items/OrderPackageRepository.java
@@ -7,8 +7,10 @@ import java.util.List;
 
 import com.wms.wms_server.model.items.OrderPackage;
 
+import java.util.*;
 
 @Repository
 public interface OrderPackageRepository extends JpaRepository<OrderPackage, Long> {
-    
+
+    List<OrderPackage> findByComplete(int complete);
 }
