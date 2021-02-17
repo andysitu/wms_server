a51e3202285f20eb5bad4cb0e3277b27463c20f1
Andy Situ <and.situ@gmail.com>
2021-02-16 20:46:15 -0800

    Add LocationReposistory.findByWarehouseId

--- src/main/java/com/wms/wms_server/repository/LocationRepository.java
+++ src/main/java/com/wms/wms_server/repository/LocationRepository.java
@@ -3,6 +3,7 @@ package com.wms.wms_server.repository;
 import org.springframework.stereotype.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.*;
 
@@ -13,4 +14,7 @@ public interface LocationRepository extends JpaRepository<Location, Integer> {
     List<Location> findByAreaId(Long areaId);
     Optional<Location> findByIdAndAreaId(Integer id, Long areaId);
     List<Location> findByLocationCode(String locationCode);
+
+    @Query("SELECT l FROM Location l inner join l.area a inner join a.warehouse w where w.id = :warehouseId")
+    List<Location> findByWarehouseId(Long warehouseId);
 }
