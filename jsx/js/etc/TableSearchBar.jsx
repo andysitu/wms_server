4c92284365cfe9a1a19ce06420161959753d354e
Andy Situ <and.situ@gmail.com>
2021-02-18 13:29:14 -0800

    Change TableSearchBar to contain form element

--- jsx/js/etc/TableSearchBar.jsx
+++ jsx/js/etc/TableSearchBar.jsx
@@ -36,7 +36,8 @@ class TableSearchBar extends React.Component {
     }
   }
 
-  onClick_search = () => {
+  onSearch = (e) => {
+    e.preventDefault();
     var search_type = document.getElementById("item-search1-type-select").value,
         search_value = document.getElementById("search-bar1-input").value;
     this.props.onSearch(search_type, search_value);
@@ -45,18 +46,21 @@ class TableSearchBar extends React.Component {
   render() {
     var search_types = this.get_search_types(this.props.search_type);
     return (
-    <div className="col-sm-10 col-md-5 input-group">
-      <input className="form-control" type="text" id="search-bar1-input" autoFocus></input>
-      <select className="custom-select col-4" id="item-search1-type-select">
-        {search_types.map(search_type=> {
-          return (<option key={"search-option-" + search_type.value}
-            value={search_type.value}>{search_type.name}</option>);
-        })}
-      </select>
-      <button className="btn btn-outline-secondary"
-        onClick={this.onClick_search}>
-        Search
-      </button>
+    <div className="col-sm-10 col-md-5">
+      <form onSubmit={this.onSearch} className="form-inline">
+        <input className="form-control" type="text" id="search-bar1-input" autoFocus></input>
+        <select className="custom-select" id="item-search1-type-select">
+          {search_types.map(search_type=> {
+            return (<option key={"search-option-" + search_type.value}
+              value={search_type.value}>{search_type.name}</option>);
+          })}
+        </select>
+        <button className="btn btn-outline-secondary"
+          type="submit"
+          onClick={this.onSearch}>
+          Search
+        </button>
+      </form>
     </div>);
   }
 }
