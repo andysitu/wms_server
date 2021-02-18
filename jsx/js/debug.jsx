92bf1eec094ff57eb99aacc5e005eda8a15e229f
Andy Situ <and.situ@gmail.com>
2021-02-17 21:32:51 -0800

    Add viewLocations & create Item Info to debug.jsx

--- jsx/js/debug.jsx
+++ jsx/js/debug.jsx
@@ -85,10 +85,56 @@ class DebugApp extends React.Component {
     });
   }
 
+  viewLocations = () => {
+    $.ajax({
+      type: "GET",
+      url: "/locations",
+      success: function(data) {
+        console.log(data);
+      }
+    });
+  }
+
+  createDummyItemInfos = () => {
+    let total = 5;
+    for (let i=1; i<=total; i++) {
+      setTimeout(()=>{
+        let data = {
+          description: this.getRandomLetters(10),
+          height: this.getRandomInt(1),
+          length: this.getRandomInt(1),
+          width: this.getRandomInt(1),
+          weight: this.getRandomInt(2),
+          itemSku: this.getRandomWordsInts(5),
+          name: this.getRandomLetters(6),
+        };
+        const count = i;
+        $.ajax({
+          url: "/item_info",
+          type: "POST",
+          data: data,
+          success: function() {
+            if (count == total) {
+              window.alert("done");
+            }
+          }
+        });
+      }, i*200);
+    }
+  }
+
   render() {
     return (<div>
+    <div>
       <button type="button"
         onClick={this.createWarehouseLocations}>Create Warehouse + Locations</button>
+      </div>
+      <div>
+        <button type="button" onClick={this.viewLocations}>View Locations</button>
+      </div>
+      <div>
+        <button type="button" onClick={this.createDummyItemInfos}>Create 5 ItemInfos</button>
+      </div>      
     </div>)
   }
 }
