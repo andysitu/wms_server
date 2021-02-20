2fdfbdde8a6ba31f72720ca83572131be812284f
Andy Situ <and.situ@gmail.com>
2021-02-19 16:24:06 -0800

    ship_order loads orders & then creates itemsList for the orders

--- jsx/js/items/ship_order.jsx
+++ jsx/js/items/ship_order.jsx
@@ -3,9 +3,36 @@ class ShipOrderApp extends React.Component {
     super(props);
     this.state = {
       openOrders: [],
+      selectedOrderIndex: -1,
     };
     this.loadOpenOrders();
   }
+
+  convertOrderItems = (order) => {
+    const itemIndexMap = {};
+    const itemsList = [];
+    let itemSku, index;
+    order.itemOrderResponses.forEach(itemResponse => {
+      itemSku = itemResponse.itemInventoryResponse.itemSku;
+      if (itemSku in itemIndexMap) {
+        index = itemIndexMap[itemSku];
+        itemsList[index].orderedQuantity += itemResponse.orderedQuantity;
+        itemsList[index].startQuantity += itemResponse.startQuantity;
+        itemsList[index].completeQuantity += itemResponse.completeQuantity;
+      } else {
+        itemIndexMap[itemSku] = itemsList.length;
+        itemsList.push({
+          itemName: itemResponse.itemInventoryResponse.itemName,
+          itemSku: itemResponse.itemInventoryResponse.itemSku,
+          orderedQuantity: itemResponse.orderedQuantity,
+          startQuantity: itemResponse.startQuantity,
+          completeQuantity: itemResponse.completeQuantity,
+          description: itemResponse.itemInventoryResponse.itemDescription,
+        });
+      }
+    });
+    return itemsList;
+  };
 
   loadOpenOrders = () => {
     $.ajax({
@@ -14,24 +41,61 @@ class ShipOrderApp extends React.Component {
       context: this,
       success: function(orders) {
         console.log(orders);
+        for(let i=0; i< orders.length; i++) {
+          orders[i].itemsList = this.convertOrderItems(orders[i]);
+        }
         this.setState({
           openOrders: orders,
+          selectedOrderIndex: -1,
         });
       }
     })
   };
 
   render() {
+    let numOpenItems, totalItems, i, trClass;
     return (
     <div>
       Ship Order
-
-      {this.state.openOrders.map(order=> {
-        return (
-        <div key={order.id}>
-          {order.orderName}
-        </div>);
-      })}
+      <table className="table table-sm">
+        <thead>
+          <tr>
+            <th scope="col">Order Name</th>
+            <th scope="col">Company Name</th>
+            <th scope="col">Transport</th>
+            <th scope="col">Unpicked Items</th>
+            <th scope="col">Total Items</th>
+            <th scope="col">Select</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.state.openOrders.map(((orderPackage, index) => {
+            // numOpenItems = 0;
+            // totalItems = 0;
+            // for (i=0; i<orderPackage.itemsList.length; i++) {
+            //   numOpenItems += orderPackage.itemsList[i].orderedQuantity;
+              
+            //   totalItems += orderPackage.itemsList[i].startQuantity;
+            // }
+            trClass = (index == this.state.selectedOrderIndex) ?
+              "selected" : "";
+            return (
+              <tr key={orderPackage.id} className={trClass}>
+                <td>{orderPackage.orderName}</td>
+                <td>{orderPackage.companyName}</td>
+                <td>{orderPackage.transportName}</td>
+                <td>{numOpenItems}</td>
+                <td>{totalItems}</td>
+                <td>
+                  <button type="button"
+                    value={index} onClick={this.onClick_selectOrder}
+                    className="btn btn-sm btn-outline-primary"
+                  >Select</button>
+                </td>
+              </tr>)
+          }))}
+        </tbody>
+      </table>
     </div>
     );
   }
