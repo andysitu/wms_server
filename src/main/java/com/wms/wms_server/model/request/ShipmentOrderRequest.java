40ea833abe5efab2ada4f5ae5756cabd23474423
Andy Situ <and.situ@gmail.com>
2021-03-01 15:06:38 -0800

    Create ShipmentOrderRequest.java

---
+++ src/main/java/com/wms/wms_server/model/request/ShipmentOrderRequest.java
@@ -0,0 +1,19 @@
+package com.wms.wms_server.model.request;
+
+public class ShipmentOrderRequest {
+    public String contactName;
+    public String companyName;
+    public String address1;
+    public String address2;
+    public String city;
+    public String state;
+    public String zip;
+    public String phone;
+    public String email;
+
+    public String tracking;
+    public String transportName;
+    public String shipmentType;
+
+    public ShipmentItemRequest[] items;
+}
