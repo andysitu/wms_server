e47033f587226252519ec9fae305cfa407e25665
Andy Situ <and.situ@gmail.com>
2021-03-01 14:55:16 -0800

    Create ShipmentUnitRequest.java

---
+++ src/main/java/com/wms/wms_server/model/request/ShipmentUnitRequest.java
@@ -0,0 +1,9 @@
+package com.wms.wms_server.model.request;
+
+public class ShipmentUnitRequest {
+    public int quantity;
+    public int weight;
+    public int length;
+    public int height;
+    public int width;
+}
