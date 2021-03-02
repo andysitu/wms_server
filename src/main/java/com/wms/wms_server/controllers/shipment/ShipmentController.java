058ac5bf2cda55bef3bb9447ed06ad94c05ec67f
Andy Situ <and.situ@gmail.com>
2021-03-01 16:47:53 -0800

    Update ShipmentController.java with ShipmentService

--- src/main/java/com/wms/wms_server/controllers/shipment/ShipmentController.java
+++ src/main/java/com/wms/wms_server/controllers/shipment/ShipmentController.java
@@ -1,7 +1,9 @@
 package com.wms.wms_server.controllers.shipment;
 
 import com.wms.wms_server.model.request.ShipmentRequest;
+import com.wms.wms_server.services.shipments.ShipmentService;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,12 +12,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class ShipmentController {
+    @Autowired
+    ShipmentService shipmentService;
+
     @RequestMapping(path="/shipments", produces="application/json",
             method = RequestMethod.POST, consumes="application/json")
     @ResponseBody
     public String createShipment(@RequestBody ShipmentRequest shipmentOrderRequest) {
-        System.out.println(shipmentOrderRequest.items.length);
-        System.out.println(shipmentOrderRequest.units.length);
+        shipmentService.processShipment(shipmentOrderRequest);
         return "HEY";
     }
 }
