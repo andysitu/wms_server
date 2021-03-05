2ac3282c56905790518d5e8c69d9767a926afd3b
Andy Situ <and.situ@gmail.com>
2021-03-04 17:49:38 -0800

    Show blank view_shipments page

--- src/main/java/com/wms/wms_server/controllers/shipment/ShipmentController.java
+++ src/main/java/com/wms/wms_server/controllers/shipment/ShipmentController.java
@@ -5,6 +5,7 @@ import com.wms.wms_server.services.shipments.ShipmentService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -22,5 +23,10 @@ public class ShipmentController {
     public String createShipment(@RequestBody ShipmentData shipmentOrderRequest) {
         shipmentService.processShipment(shipmentOrderRequest);
         return "HEY";
+    }
+
+    @GetMapping("/view")
+    public String viewShipmentPage() {
+        return "/shipments/view_shipments";
     }
 }
