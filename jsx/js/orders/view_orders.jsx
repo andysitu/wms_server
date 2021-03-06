789f92e7ac0e8ef058d9ac0f8c43e9f26717e869
Andy Situ <and.situ@gmail.com>
2021-03-05 17:01:20 -0800

    Add temporary orders table to view orders

--- jsx/js/orders/view_orders.jsx
+++ jsx/js/orders/view_orders.jsx
@@ -1,8 +1,10 @@
 class OrderShipmentApp extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
-    // this.loadShipments();
+    this.state = {
+      orders: [],
+    };
+    this.loadShipments();
     this.loadOrders();
   }
 
@@ -17,6 +19,35 @@ class OrderShipmentApp extends React.Component {
     })
   }
 
+  convertOrderItems = (order) => {
+    const itemIndexMap = {};
+    const itemsList = [];
+    let itemSku, index;
+    order.itemOrderResponses.forEach(itemResponse => {
+      itemSku = itemResponse.itemInventoryResponse.itemSku;
+      if (itemSku in itemIndexMap) {
+        index = itemIndexMap[itemSku];
+        itemsList[index].orderedQuantity += itemResponse.orderedQuantity;
+        itemsList[index].startQuantity += itemResponse.startQuantity;
+        itemsList[index].completeQuantity += itemResponse.completeQuantity;
+        itemsList[index].pickedQuantity += itemResponse.pickedQuantity;
+      } else {
+        itemIndexMap[itemSku] = itemsList.length;
+        itemsList.push({
+          id: itemResponse.id,
+          itemName: itemResponse.itemInventoryResponse.itemName,
+          itemSku: itemResponse.itemInventoryResponse.itemSku,
+          orderedQuantity: itemResponse.orderedQuantity,
+          startQuantity: itemResponse.startQuantity,
+          pickedQuantity: itemResponse.pickedQuantity,
+          completeQuantity: itemResponse.completeQuantity,
+          description: itemResponse.itemInventoryResponse.itemDescription,
+        });
+      }
+    });
+    return itemsList;
+  };
+
   loadOrders = () => {
     $.ajax({
       url: "/orderpackages",
@@ -24,20 +55,79 @@ class OrderShipmentApp extends React.Component {
       context: this,
       success: function(orders) {
         console.log(orders);
-        // for(let i=0; i< orders.length; i++) {
-        //   orders[i].itemsList = this.convertOrderItems(orders[i]);
-        // }
-        // this.setState({
-        //   openOrders: orders,
-        //   selectedOrderIndex: -1,
-        // });
+        for(let i=0; i< orders.length; i++) {
+          orders[i].itemsList = this.convertOrderItems(orders[i]);
+        }
+        this.setState({
+          orders: orders,
+          selectedOrderIndex: -1,
+        });
       }
     })
+  };
+
+  onClick_selectOrder = () => {
+
+  }
+
+  createOrdersMenu = () => {
+    let numOpenItems, totalItems, numPickedItems,
+        i, trClass;
+    return (
+    <div id={this.ordersContainerId}>
+      <h2>Orders</h2>
+      <div id="open-orders-container">
+        <table className="table table-sm">
+          <thead>
+            <tr>
+              <th scope="col">Order Name</th>
+              <th scope="col">Company Name</th>
+              <th scope="col">Transport</th>
+              <th scope="col">Unpicked Items</th>
+              <th scope="col">Picked Items</th>
+              <th scope="col">Total Items</th>
+              <th scope="col">Select</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.orders.map(((orderPackage, index) => {
+              numOpenItems = 0;
+              totalItems = 0;
+              numPickedItems = 0;
+              orderPackage.itemsList.forEach(item => {
+                numOpenItems += item.orderedQuantity;
+                totalItems += item.startQuantity;
+                numPickedItems += item.pickedQuantity;
+              });
+              trClass = (index == this.state.selectedOrderIndex) ?
+                "selected" : "";
+              return (
+                <tr key={orderPackage.id} className={trClass}>
+                  <td>{orderPackage.orderName}</td>
+                  <td>{orderPackage.companyName}</td>
+                  <td>{orderPackage.transportName}</td>
+                  <td>{numOpenItems}</td>
+                  <td>{numPickedItems}</td>
+                  <td>{totalItems}</td>
+                  <td>
+                    <button type="button"
+                      value={index} onClick={this.onClick_selectOrder}
+                      className="btn btn-sm btn-outline-primary"
+                    >Select</button>
+                  </td>
+                </tr>)
+            }))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+    );
   };
 
   render() {
     return (<div>
       Orders
+      {this.createOrdersMenu()}
     </div>);
   }
 }
