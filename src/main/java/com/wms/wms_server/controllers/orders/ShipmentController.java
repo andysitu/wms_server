5182cb231dda66748041f15369a56f5bad47da59
Andy Situ <and.situ@gmail.com>
2021-03-08 15:36:03 -0800

    Add getShipment to Shipment Controller

--- src/main/java/com/wms/wms_server/controllers/orders/ShipmentController.java
+++ src/main/java/com/wms/wms_server/controllers/orders/ShipmentController.java
@@ -8,6 +8,7 @@ import com.wms.wms_server.services.shipments.ShipmentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -31,5 +32,13 @@ public class ShipmentController {
     public String createShipment(@RequestBody ShipmentData shipmentOrderRequest) {
         shipmentService.processShipment(shipmentOrderRequest);
         return "OK";
+    }
+
+    @RequestMapping(path="/{shipmentId}", produces="application/json",
+            method = RequestMethod.GET, consumes="application/json")
+    @ResponseBody
+    public ShipmentData getShipment(@PathVariable("shipmentId") Long shipmentId) {
+        ShipmentData data = shipmentService.getShipmentData(shipmentId);
+        return data;
     }
 }
