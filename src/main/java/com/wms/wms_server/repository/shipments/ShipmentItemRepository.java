e8b957694a92778a84a2a3807060773d34bdb118
Andy Situ <and.situ@gmail.com>
2021-03-08 15:35:32 -0800

    Update ShipmentItemRepository.java

--- src/main/java/com/wms/wms_server/repository/shipments/ShipmentItemRepository.java
+++ src/main/java/com/wms/wms_server/repository/shipments/ShipmentItemRepository.java
@@ -5,7 +5,9 @@ import com.wms.wms_server.model.shipment.ShipmentItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.*;
+
 @Repository
 public interface ShipmentItemRepository extends JpaRepository<ShipmentItem, Long> {
-    
+    List<ShipmentItem> findByShipmentId(Long shipmentId);
 }
