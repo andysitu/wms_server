af7898a6892cff3f2c2ee4bfe212e7e3d07f521f
Andy Situ <and.situ@gmail.com>
2021-03-09 14:04:14 -0800

    Update ShipmentItemData.java

--- src/main/java/com/wms/wms_server/model/request/ShipmentItemData.java
+++ src/main/java/com/wms/wms_server/model/request/ShipmentItemData.java
@@ -3,5 +3,5 @@ package com.wms.wms_server.model.request;
 public class ShipmentItemData {
     public long id;
     public String itemSku;
-    public int shippingQuantity;
+    public int quantity;
 }
