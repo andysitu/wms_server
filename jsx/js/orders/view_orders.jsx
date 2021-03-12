88ce06e6f3c17d14182fbe2fdb0d6f45a952daab
Andy Situ <and.situ@gmail.com>
2021-03-11 17:42:11 -0800

    Add shipment table to view orders page

--- jsx/js/orders/view_orders.jsx
+++ jsx/js/orders/view_orders.jsx
@@ -150,14 +150,43 @@ class OrderShipmentApp extends React.Component {
           <div>
             <h2>Shipments</h2>
 
-          {shipments.map(shipment => {
-            return (
-            <div key={shipment.id}>
-              <button onClick={this.onClick_viewShipment} value={shipment.id}>
-                {shipment.id}
-              </button>
-            </div>)
-          })}
+            <table className="table table-sm">
+              <thead>
+                <tr>
+                  <th scope="col">City</th>
+                  <th scope="col">State</th>
+                  <th scope="col">Transport</th>
+                  <th scope="col">Tracking</th>
+                  <th scope="col">Select</th>
+                </tr>
+              </thead>
+              <tbody>
+                {shipments.map(shipment => {
+                  return (
+                  <tr key={shipment.id}>
+                    <td>
+                      {shipment.city}
+                    </td>
+                    <td>
+                      {shipment.state}
+                    </td>
+                    <td>
+                      {shipment.transportName}
+                    </td>
+                    <td>
+                      {shipment.tracking}
+                    </td>
+                    <td>
+                      <button onClick={this.onClick_viewShipment} value={shipment.id}>
+                        {shipment.id}
+                      </button>
+                    </td>
+                  </tr>)
+                })}
+              </tbody>
+            </table>
+          </div>
+          
         </div>
       </div>)
     } else {
