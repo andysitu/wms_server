e08c2d781dc3bde4db341660640e15754f108e98
Andy Situ <and.situ@gmail.com>
2021-03-24 16:06:33 -0700

    Update OrderPackageRepository.java

--- src/main/java/com/wms/wms_server/repository/items/OrderPackageRepository.java
+++ src/main/java/com/wms/wms_server/repository/items/OrderPackageRepository.java
@@ -3,7 +3,6 @@ package com.wms.wms_server.repository.items;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import java.util.List;
 
 import com.wms.wms_server.model.items.OrderPackage;
 
@@ -11,6 +10,5 @@ import java.util.*;
 
 @Repository
 public interface OrderPackageRepository extends JpaRepository<OrderPackage, Long> {
-
     List<OrderPackage> findByComplete(int complete);
 }
